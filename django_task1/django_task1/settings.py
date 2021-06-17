dca0fe79246eada9ffdfdb69a8e7feab04f2eea7
Nazar Mazur <nazarii.mazur@gmail.com>
2021-06-17 20:44:51 +0300

    add config for database,all proc for init project takes 40min

--- django_task1/django_task1/settings.py
+++ django_task1/django_task1/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'django_task1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from db import DATABASES
 
 
 # Password validation
