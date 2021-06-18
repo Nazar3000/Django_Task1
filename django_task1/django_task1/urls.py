8079f15a127f1264c111c7c5ed1f29e02f1a1673
Nazar Mazur <nazarii.mazur@gmail.com>
2021-06-18 22:42:57 +0300

    remove old migrations

--- django_task1/django_task1/urls.py
+++ django_task1/django_task1/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task_app.views import FeedItemView, FeedItemUserView
+from task_app.views import FeedItemView, FeedItemUserView, RelatedItemUserView
 
 urlpatterns = [
     path('', FeedItemView.as_view(), name='posts'),
     path('selfposts/', FeedItemUserView.as_view(), name='posts'),
+    path('relatedposts/', RelatedItemUserView.as_view(), name='posts'),
     path('admin/', admin.site.urls),
 
 ]
--- django_task1/task_app/views.py
+++ django_task1/task_app/views.py
@@ -29,3 +29,25 @@ class FeedItemUserView(ListView):
     #     FeedItem.objects.filter(user=request.user)
     # queryset = FeedItem.objects.filter(user=request.user)
 
+class RelatedItemUserView(ListView):
+    model = FeedItem
+    template_name = 'task_app/posts.html'
+    context_object_name = 'publisher'
+
+    def get_queryset(self):
+        if self.request.user.is_authenticated:
+            # user= FeedItem.objects.filter(user=self.request.user)
+            user = RegisteredUser.objects.filter(id=self.request.user.id).values("user_id")
+            # RegisteredUser.
+            # tracking = user.tracking.get().values("user_id")
+            tracked_users = RegisteredUser.objects.filter(tracked_by__user=user).values("user_id")
+            # tracking = user1.objects.all().values("user_id")
+            # tracking = RegisteredUser.tracking.all().values("user_id")
+
+            queryset = FeedItem.objects.filter(user_id__in=tracked_users)
+
+
+            # queryset = FeedItem.objects.filter(user_id__in=tracked_users)
+            # queryset1= queryset.all()
+            # queryset=FeedItem.objects.filter(user_id__in=user.tracking.all().values("user_id"))
+            return queryset
