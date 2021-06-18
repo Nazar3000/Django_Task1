a2d30d75ccfa78e25d04524258aa1a7ee781ed8f
Nazar Mazur <nazarii.mazur@gmail.com>
2021-06-19 00:22:13 +0300

    finished RelatedItemUserView it takes 2h

--- django_task1/task_app/views.py
+++ django_task1/task_app/views.py
@@ -24,10 +24,7 @@ class FeedItemUserView(ListView):
         if self.request.user.is_authenticated:
             queryset=FeedItem.objects.filter(user=self.request.user)
             return queryset
-        # return FeedItem.objects.filter(user=self.request.user)
-    # if request.user.is_authenticated:
-    #     FeedItem.objects.filter(user=request.user)
-    # queryset = FeedItem.objects.filter(user=request.user)
+
 
 class RelatedItemUserView(ListView):
     model = FeedItem
@@ -36,18 +33,7 @@ class RelatedItemUserView(ListView):
 
     def get_queryset(self):
         if self.request.user.is_authenticated:
-            # user= FeedItem.objects.filter(user=self.request.user)
-            user = RegisteredUser.objects.filter(id=self.request.user.id).values("user_id")
-            # RegisteredUser.
-            # tracking = user.tracking.get().values("user_id")
-            tracked_users = RegisteredUser.objects.filter(tracked_by__user=user).values("user_id")
-            # tracking = user1.objects.all().values("user_id")
-            # tracking = RegisteredUser.tracking.all().values("user_id")
-
+            user = RegisteredUser.objects.filter(user_id=self.request.user.id).values("user_id")
+            tracked_users = RegisteredUser.objects.filter(tracking__user_id__in=user).values("user_id")
             queryset = FeedItem.objects.filter(user_id__in=tracked_users)
-
-
-            # queryset = FeedItem.objects.filter(user_id__in=tracked_users)
-            # queryset1= queryset.all()
-            # queryset=FeedItem.objects.filter(user_id__in=user.tracking.all().values("user_id"))
             return queryset
